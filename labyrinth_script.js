1c452309c14b3a4e6370117cea4b913cc5ce78fd
DanaKharaz <danakharaz@gmail.com>
2024-04-28 02:38:30 +0200

    add items to maze

--- labyrinth_script.js
+++ labyrinth_script.js
@@ -5,15 +5,11 @@ const delay = millis => new Promise((resolve, reject) => {setTimeout(_ => resolv
 const canvas = document.querySelector('#labyrinth-canvas')
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerHeight;
-canvas.height = window.innerHeight*0.8;
+// TO-DO : to start the game must press Tab, add a simple riddle for it - reason: makes keydown work in iframe
 
-const frac = 31;
-let x = canvas.width/frac;
-let y = canvas.width/frac;
-const radius = (canvas.width/frac)*0.4;
-const offX = (canvas.width/frac)*0.1;
-const offY = (canvas.width/frac)*0.1;
+/* ********************************************************************************************** */
+
+/** RANDOM MAZE **/
 
 const size = 25;
 
@@ -76,18 +72,39 @@ createMaze(1, 1);
 maze[1][0] = 0;
 maze[size - 2][size - 1] = 0;
 
-// TO-DO : junctions and add  more complexity to the maze (collecting a few items before leaving)
 // find crossroads
-for (let col = 1; col < (frac - 6 - 1); col++) {
-    for (let row = 1; row < (frac - 6 - 1); row++) {
+for (let col = 1; col < size - 1; col++) {
+    for (let row = 1; row < size - 1; row++) {
         // path cells with more than 2 possible next steps are crossroads
         if ([maze[row - 1][col], maze[row][col - 1], 
             maze[row + 1][col], maze[row][col + 1]].filter((v) => v == 0).length > 2 && 
             maze[row][col] == 0) maze[row][col] = 8;
     }
 }
-console.log(maze);
 
+// more complexity to the maze - need to collect 5 items before leaving
+let items = [];
+while (items.length < 5) {
+    const  itemRow = Math.floor(Math.random()*size);
+    const itemCol = Math.floor(Math.random()*size);
+    if (!items.includes([itemRow, itemCol]) && maze[itemRow][itemCol] != 1) items.push([itemRow, itemCol]);
+}
+
+/* ********************************************************************************************** */
+
+/** INITIAL LAYOUT **/
+
+canvas.width = window.innerHeight;
+canvas.height = window.innerHeight*0.8;
+
+const frac = 31;
+let x = canvas.width/frac;
+let y = canvas.width/frac;
+const radius = (canvas.width/frac)*0.4;
+const offX = (canvas.width/frac)*0.1;
+const offY = (canvas.width/frac)*0.1;
+
+// draw the maze
 ctx.fillStyle = '#666666';
 ctx.fillRect(3*x, 0, (frac - 6)*x, (frac - 6)*y);
 for (let col = 0; col < (frac - 6); col++) {
@@ -98,13 +115,27 @@ for (let col = 0; col < (frac - 6); col++) {
     }
 }
 
-ctx.fillStyle = '#3b3b3b'; // for the moving circle
+// place items in the maze
+ctx.fillStyle = '#ffffff';
+items.forEach((pos) => {
+    ctx.beginPath();
+    ctx.arc((pos[1] + 4)*x - radius/2 - offX*3, (pos[0] + 1)*y - radius/2 - offY*3, radius/2, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+});
 
+// draw the moving circle at the maze entrance
+ctx.fillStyle = '#3b3b3b'; // for the moving circle
 ctx.beginPath();
 ctx.arc(2*x - radius - offX, 2*y - radius - offY, radius, 0, Math.PI * 2); // initial position
 ctx.fill();
 ctx.closePath();
 
+/* ********************************************************************************************** */
+
+/** GAME ANIMATION **/
+
+// animate circle movement activated by arrow keys
 let started = false;
 let currI = 0;
 let currJ = 1;
@@ -112,21 +143,24 @@ let prevI, prevJ;
 
 let isAnimating = false;
 
-async function drawBall(dx, dy, entering = false, exiting = false) {
+async function drawCircle(dx, dy, entering = false, exiting = false) { // TO-DO : redraw items when overlapping ???
     isAnimating = true;
 
-    if (entering) prevI = -2;
-    if (exiting) currI = frac - 6 + 1;
+    if (entering) prevI = -2; // start  from a non-existing cell before maxe entrance
+    if (exiting) currI = frac - 6 + 1; // end at  a non-existing cell after maze exit
 
+    // initial circle position
     let arcX = (prevI + 4)*x - radius - offX;
     let arcY = (prevJ + 1)*y - radius - offY;
     let clearX = (prevI + 3)*x;
     let clearY = prevJ*y;
+
+    // move circle until needed position is reached
     while (dx > 0 && arcX <= (currI + 4)*x - radius - offX ||
            dx < 0 && arcX >= (currI + 4)*x - radius - offX ||
            dy > 0 && arcY <= (currJ + 1)*y - radius - offY ||
            dy < 0 && arcY >= (currJ + 1)*y - radius - offY) {
-        await delay(5);
+        await delay(3);
         ctx.clearRect(clearX, clearY, x, y);
         arcX += dx;
         arcY += dy;
@@ -137,21 +171,30 @@ async function drawBall(dx, dy, entering = false, exiting = false) {
         ctx.fill();
         ctx.closePath();
     }
-    // final position
+    // draw at final position (in case of discreptancies due to adding/subtracting steps)
     ctx.clearRect((currI + 3)*x, currJ*y, x, y);
     ctx.beginPath();
-    ctx.arc((currI + 4)*x - radius - offX, (currJ + 1)*y - radius - offY, radius, 0, Math.PI * 2); // initial position
+    ctx.arc((currI + 4)*x - radius - offX, (currJ + 1)*y - radius - offY, radius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
+
     isAnimating = false;
 }
-async function draw(event) {
+
+window.addEventListener('keydown', circleMovement);
+
+async function circleMovement(event) {
     if (isAnimating) return; // should not interrupt current animation
 
+    // initinal position
     prevI = currI;
     prevJ = currJ;
+
     const step = 2;
 
+    const itemsNew = [];
+
+    // move based on pressed key
     switch(event.code) {
         case 'ArrowRight':
             if (maze[currJ][currI + 1] === 1) break; // can't go in this direction because of a wall
@@ -159,39 +202,66 @@ async function draw(event) {
             while (maze[currJ][currI] === 0) currI++;
             if (maze[currJ][currI] != 8) currI--; // will go into a wall otherwise
             if (!started) {
-                drawBall(step, 0, true);
+                drawCircle(step, 0, true);
                 started = true;
             }
-            else if (currI == (frac - 6 - 1) && currJ == (frac - 6 - 2)) { // TO-DO : after exit found
-                drawBall(step, 0, false, true);
+            else if (currI == (frac - 6 - 1) && currJ == (frac - 6 - 2)) {
+                drawCircle(step, 0, false, true);
+                mazeSolved();
             } else {
-                drawBall(step, 0);
+                drawCircle(step, 0);
             }
+
+            for (let k = 0; k < items.length; k++) { // check if any items were collected
+                if (!(prevI < items[k][1] && items[k][1] <= currI && currJ === items[k][0])) itemsNew.push(items[k]);
+            }
+            items = itemsNew;
+
             break;
         case 'ArrowDown':
             if (maze[currJ + 1][currI] === 1) break; // can't go in this direction because of a wall
             currJ++; // start moving (here beacause loop below won't move from crossroad)
             while (maze[currJ][currI] === 0) currJ++;
             if (maze[currJ][currI] != 8) currJ--; // will go into a wall otherwise
-            drawBall(0, step);
+            drawCircle(0, step);
+
+            for (let k = 0; k < items.length; k++) { // check if any items were collected
+                if (!(prevJ < items[k][0] && items[k][0] <= currJ && currI === items[k][1])) itemsNew.push(items[k]);
+            }
+            items = itemsNew;
+
             break;
         case 'ArrowLeft':
             if (maze[currJ][currI - 1] === 1 || (currI == 0 && currJ == 1)) break; // can't go in this direction because of a wall
             currI--; // start moving (here beacause loop below won't move from crossroad)
             while (maze[currJ][currI] === 0) currI--;
             if (maze[currJ][currI] != 8) currI++; // will go into a wall otherwise
-            drawBall(-step, 0);
+            drawCircle(-step, 0);
+
+            for (let k = 0; k < items.length; k++) { // check if any items were collected
+                if (!(prevI > items[k][1] && items[k][1] >= currI && currJ === items[k][0])) itemsNew.push(items[k]);
+            }
+            items = itemsNew;
+
             break;
         case 'ArrowUp':
             if (maze[currJ - 1][currI] === 1) break; // can't go in this direction because of a wall
             currJ--; // start moving (here beacause loop below won't move from crossroad)
             while (maze[currJ][currI] === 0) currJ--;
             if (maze[currJ][currI] != 8) currJ++; // will go into a wall otherwise
-            drawBall(0, -step);
+            drawCircle(0, -step);
+
+            for (let k = 0; k < items.length; k++) { // check if any items were collected
+                if (!(prevJ > items[k][0] && items[k][0] >= currJ && currI === items[k][1])) itemsNew.push(items[k]);
+            }
+            items = itemsNew;
+
             break;
     }
 }
 
-// TO-DO : to start the game must press Tab, add a simple riddle for it - reason: makes keydown work in iframe
-
-window.addEventListener('keydown', draw);
+function mazeSolved() {
+    if (items.length === 0) console.log('game won');
+    else console.log('game lost');
+    // TO-DO : transtion to next part of the game
+}
