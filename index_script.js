6972e7675c7e47bee211a9145e63b599ac8423a5
DanaKharaz <danakharaz@gmail.com>
2024-05-17 18:09:02 +0200

    add text effect to intro text

--- index_script.js
+++ index_script.js
@@ -25,7 +25,7 @@ async function titleBlink() {
         title.textContent = 'MIS\u00A0ERY';
     }
 
-    /*await delay(500);
+    await delay(500);
     for (let i = 0; i < 5; i++) {
         await delay(80);
         title.textContent = '';
@@ -33,9 +33,8 @@ async function titleBlink() {
         title.textContent = 'MIS\u00A0ERY';
     }
 
-    await delay(500);*/
+    await delay(500);
     title.style.display = 'none';
-    //await delay(500);
 
     newLine(true);
     showIntroText();
@@ -47,8 +46,7 @@ const introScreen = document.querySelector('#intro-screen');
 
 const introLines = [];
 // TODO : write intro text
-//const introText = "hello,\u00A0you.\u00A0how\u00A0did\u00A0you\u00A0end\u00A0up\u00A0here?\u00A0well..\u00A0there\u00A0is\u00A0no\u00A0escape\u00A0now\u00A01\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A01\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A02\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A03\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A04\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A05\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A06\u00A02345678\u00A07\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A08\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A09";
-const introText = 'sample\u00A0text\u00A0123456789'
+const introText = "hello,\u00A0you.\u00A0how\u00A0did\u00A0you\u00A0end\u00A0up\u00A0here?\u00A0well..\u00A0there\u00A0is\u00A0no\u00A0escape\u00A0now\u00A0(instructions)\n\u00A0I'll\u00A0give\u00A0you\u00A0a\u00A0hint:\u00A0read\u00A0between\u00A0the\u00A0lines\u00A0stars";
 const introTextWords = introText.split('\u00A0');
 
 const maxCharPerLine = 66;
@@ -58,16 +56,22 @@ let lineI = 0;
 let charInLines = 0;
 
 // add longer pauses after certain characters
-const charPauses = []; // cantains pauses in millis
-for (let i = 0; i < introText.length; i++) charPauses.push(100); // default value
+const defaultPause = 100;
+const charPauses = new Array(introText.length).fill(defaultPause); // cantains pauses in millis (100 by default)
 // TODO : add needed pauses
 charPauses[6] = 1000;
 charPauses[11] = 1000;
 charPauses[36] = 1000;
 charPauses[43] = 1000;
 charPauses[67] = 1000;
+charPauses[126] = 1000;
 
-//FIXMEnewLine(true);
+// add html text effects
+// strikethrough effect {index of last char affected : length affected} - the word is written then striked
+const charStrikes = {};
+charStrikes[125] = 5;
+
+newLine(true);
 function newLine(starting = false) {
     if (!starting) introLines[lineI].style.animation = 'none'; // remove blinking bar from previous line
     const l = document.createElement('div');
@@ -76,47 +80,69 @@ function newLine(starting = false) {
     introLines.push(l);
 }
 
-//FIXMEshowIntroText();
 async function showIntroText() {
     while (charI < introText.length) {
-        //await delay(charPauses[charI]);
-        await delay(0); // FIXME test
+        await delay(charPauses[charI]);
+        //await delay(20); // FIXME test
+        
         if (introText[charI] == '\n') { // line break within text
             newLine();
             lineI++;
             charI++;
             charInLines++;
             continue;
+
         }
         if (charInLines % maxCharPerLine == 0) { // no more space, so start new line
             newLine();
             lineI++;
         }
+
         if (introText[charI] == '\u00A0') {
-            wordI++; // next word
+            wordI++; // starting next word
             
-            if (charInLines % maxCharPerLine + introTextWords[wordI].length > maxCharPerLine) { // next word will not fit, so start new line
+            // next word will not fit, so start new line
+            if (charInLines % maxCharPerLine + introTextWords[wordI].length > maxCharPerLine) {
                 newLine();
                 lineI++;
                 charInLines = 0; // exact value does not matter, as always taken modulo maxCharPerLine
             }
 
-            if (introLines[lineI].textContent.length == 0) { // should not start line with a blank space
+            // should not start line with a blank space
+            if (introLines[lineI].textContent.length == 0) { // using textContent here as it is unrelated to text effects
                 charI++;
                 charInLines++;
                 continue;
             }
         }
-        introLines[lineI].textContent += introText[charI];
-        charI++;
+
+        introLines[lineI].innerHTML += introText[charI]; // innerHTML and not textContent to allow for text effects
+
+        if (charStrikes[charI]) { // add strikethrough effect
+            await delay(charPauses[charI + 1]); // wait before srtriking out the word
+
+            // get the part before soon-to-be striked out phrase
+            const n = introLines[lineI].innerHTML.length;
+            const withoutStrike = introLines[lineI].innerHTML.substring(0, n - charStrikes[charI]);
+
+            // strike out the phrase character by character
+            const strike = introText.substring(charI + 1 - charStrikes[charI], charI + 1);
+            for (let i = 1; i < strike.length + 1; i++) {
+                introLines[lineI].innerHTML = withoutStrike + '<s>' + strike.substring(0, i) + '</s>' + strike.substring(i);
+                await delay(defaultPause);
+            }
+        }
+
+        charI++
         charInLines++;
     }
-    //FIXMEawait delay(1000);
+    await delay(1000);
 
-    // show background and start button
-    introScreen.style.display = 'none';
-    // TODO background
-    document.querySelector('#start-screen-container').classList.remove('start-screen-hidden');
+    //FIXME show background and start button
+    /*introScreen.style.display = 'none';
+    generalBackground();
+    dotGameBackground();
+    document.querySelector('#start-screen-container').classList.remove('start-screen-hidden');*/
 }
 
 /** BACKGROUND **/ //TODO
@@ -126,40 +152,43 @@ const ctx = canvas.getContext('2d');
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
-ctx.fillStyle = '#3b3b3b';
-for (let i = 0; i < 300; i++) {
-    let x, y;
-    x = Math.random() * window.innerWidth;
-    y = Math.random() * window.innerHeight;
-
-    ctx.beginPath();
-    ctx.arc(x, y, 2, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-    ctx.closePath();
-}
-ctx.fillStyle = '#666666';
-for (let i = 0; i < 400; i++) {
-    let x, y;
-    x = Math.random() * window.innerWidth;
-    y = Math.random() * window.innerHeight;
-
-    ctx.beginPath();
-    ctx.arc(x, y, 3, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-    ctx.closePath();
-}
-const img = new Image();
-img.src = 'index_res/stars.png';//h500,w850
-img.onload = function() {
+function generalBackground() {
+    ctx.fillStyle = '#666666';
     for (let i = 0; i < 200; i++) {
         let x, y;
         x = Math.random() * window.innerWidth;
         y = Math.random() * window.innerHeight;
 
-        if (Math.floor(Math.random() * 3) % 2 == 1) ctx.drawImage(img, 0, 0, 500, 500, x - 7.5, y - 7.5, 15, 15);
-        else ctx.drawImage(img, 550, 0, 300, 500, x - 4.5, y - 7.5, 9, 15);
+        ctx.beginPath();
+        ctx.arc(x, y, 2, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
+    ctx.fillStyle = '#3b3b3b';
+    ctx.strokeStyle = '#3b3b3b';
+    for (let i = 0; i < 200; i++) {
+        let x, y;
+        x = Math.random() * window.innerWidth;
+        y = Math.random() * window.innerHeight;
+
+        ctx.beginPath();
+        ctx.arc(x, y, 3, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
+    const img = new Image();
+    img.src = 'index_res/stars.png';
+    img.onload = function() {
+        for (let i = 0; i < 50; i++) {
+            let x, y;
+            x = Math.random() * window.innerWidth;
+            y = Math.random() * window.innerHeight;
+
+            if (Math.floor(Math.random() * 3) % 2 == 1) ctx.drawImage(img, 0, 0, 500, 500, x - 7.5, y - 7.5, 15, 15);
+            else ctx.drawImage(img, 550, 0, 300, 500, x - 4.5, y - 7.5, 9, 15);
+        }
     }
 }
 
@@ -184,85 +213,55 @@ async function transitionFirstGame(event) {
 /** CONNECT DOTS **/ //TODO
 
 const mainContainer = document.querySelector('#start-screen-container');
-// dictionary of dots (stars) that can be connected (will be filled from dots) - dot:number of dots it still needs to connect to (game done when all are 0)
-//const connecitons = {};
-// dictionary of dot connections {'xa,ya':['xb,yb','xc,yc'], 'xb,yb':['xa,ya'], ...] - point (xa,ya) connects to points (xb,yb) and (xc,yc)
-//const dotsInitial = {'12.18756,41.11665':['15.23271,40.68721'], '15.23271,40.68721':['12.18756,41.11665'], '2.58692,41.86383':['20.776,41.76604'], '20.776,41.76604':['2.58692,41.86383'], '11.97483,48.02465':['6.69413,35.11627'], '13.44169,46.94895':['6.69413,35.11627'], '6.69413,35.11627':['13.44169,46.94895', '11.97483,48.02465']}; // TODO
-const dots = {}; // A:[B,C]
-const dotsBack = {}; // B:A, C:A
-// define connections one way for now to not redraw same objects during set up (A:[B] included while B:[A] not included as it is redundant)
-const dotsInitial = {'12.18756,41.11665':['15.23271,40.68721'], '2.58692,41.86383':['20.776,41.76604'], '6.69413,35.11627':['13.44169,46.94895', '11.97483,48.02465']}; // TODO : 70-x and 50-y
-/*for (let i = 0; i < connections.length; i++) {
-    const x1 = Math.round(connections[i][0]/70*window.innerWidth);
-    const y1 = Math.round(connections[i][1]/50*window.innerHeight);
-    const x2 = Math.round(connections[i][2]/70*window.innerWidth);
-    const y2 = Math.round(connections[i][3]/50*window.innerHeight);
 
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.arc(x1, y1, 3, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-    ctx.closePath();
+// dictionaries of dot connections {'xa,ya':['xb,yb','xc,yc'], ...] - point (xa,ya) connects to points (xb,yb) and (xc,yc)
 
-    ctx.beginPath();
-    ctx.arc(x2, y2, 3, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-    ctx.closePath();
+// define connections one way for now to not redraw same objects during setup (A:[B] included while B:[A] not included as it is redundant)
+const dotsInitial = {'12.18756,8.88335':['15.23271,9.31279'], '2.58692,8.13617':['20.776,8.23396'], '6.69413,14.88373':['13.44169,3.05105', '11.97483,1.97535']}; // TODO : 70-x and 50-y
+// values for this window (calculated in setup)
+const dots = {}; // {A:[B,C], ...}
+const dotsBack = {}; // {B:A, C:A, ...}
 
-    ctx.strokeStyle='green'; //FIXME
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-    ctx.closePath();
+function dotGameBackground() {
+    for (const d in dotsInitial) { // set up the dots on canvas
+        let dot = d.split(',');
+        const x1 = Math.round(parseFloat(dot[0])/70*window.innerWidth);
+        const y1 = Math.round(parseFloat(dot[1])/50*window.innerHeight);
 
-    if (!dots.includes([x1, y1])) dots.push([x1, y1]);
-    if (!dots.includes([x2, y2])) dots.push([x2, y2]);
-
-    connections[i] = [x1, y1, x2, y2];
-}*/
-
-ctx.strokeStyle = 'red'; // FIXME
-
-for (const d in dotsInitial) { // set up the dots on canvas
-    let dot = d.split(',');
-    const x1 = Math.round(parseFloat(dot[0])/70*window.innerWidth);
-    const y1 = Math.round(parseFloat(dot[1])/50*window.innerHeight);
-
-    ctx.fillStyle = 'red'; // FIXME
-    ctx.beginPath();
-    ctx.arc(x1, y1, 3, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-    ctx.closePath();
-
-    dots[x1+','+y1] = [];
-
-    for (let i = 0; i < dotsInitial[d].length; i++) {
-        dot = dotsInitial[d][i].split(',');
-        const x2 = Math.round(parseFloat(dot[0])/70*window.innerWidth);
-        const y2 = Math.round(parseFloat(dot[1])/50*window.innerHeight);
-
+        ctx.fillStyle = '#3b3b3b';
         ctx.beginPath();
-        ctx.arc(x2, y2, 3, 0, 2 * Math.PI);
+        ctx.arc(x1, y1, 3, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
 
-        dots[x1+','+y1].push(x2+','+y2);
+        dots[x1+','+y1] = [];
 
-        if (dotsBack[x2+','+y2]) dotsBack[x2+','+y2].push(x1+','+y1);
-        else dotsBack[x2+','+y2] = [x1+','+y1];
+        for (let i = 0; i < dotsInitial[d].length; i++) {
+            dot = dotsInitial[d][i].split(',');
+            const x2 = Math.round(parseFloat(dot[0])/70*window.innerWidth);
+            const y2 = Math.round(parseFloat(dot[1])/50*window.innerHeight);
+            console.log((70-parseFloat(dot[0])) + ',' + (50-parseFloat(dot[1])));
+
+            ctx.beginPath();
+            ctx.arc(x2, y2, 3, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fill();
+            ctx.closePath();
+
+            dots[x1+','+y1].push(x2+','+y2);
+
+            if (dotsBack[x2+','+y2]) dotsBack[x2+','+y2].push(x1+','+y1);
+            else dotsBack[x2+','+y2] = [x1+','+y1];
+        }
     }
+    Object.assign(dots, dotsBack); // merge the dictionaries for easier searching later
 }
-Object.assign(dots, dotsBack); // merge the dictionaries for easier searching later
 
 const clicked = [];
 const redrawn = []; // array of coordinates [[x,y], ...]
-window.addEventListener('mousemove', async function(event) {
-    ctx.fillStyle = 'red'; // FIXME
+window.addEventListener('mousemove', function(event) {
+    ctx.fillStyle = '#3b3b3b';
     for (const [x, y] of redrawn) { // redraw previously highlighted in default color
         ctx.beginPath();
         ctx.arc(x, y, 3, 0, 2 * Math.PI);
@@ -288,7 +287,7 @@ window.addEventListener('mousemove', async function(event) {
                 const x2 = parseInt(dot[0]);
                 const y2 = parseInt(dot[1]);
 
-                ctx.fillStyle = 'blue';
+                ctx.fillStyle = '#ffffff';
                 ctx.beginPath();
                 ctx.arc(x2, y2, 3, 0, 2 * Math.PI);
                 ctx.stroke();
